0ed70c90a907caa307465de68794742edbf129a8
JuanMPaz <juanmpaz91@gmail.com>
2022-07-26 16:04:37 -0300

    main questions

---
+++ js/questions.js
@@ -0,0 +1,22 @@
+(function() {
+    // contante para obtener todos las preguntas. Los tres puntos y corchetes son para convertirlo en un Array
+    const titleQuestions = [...document.querySelectorAll('.questions__title')];
+
+    titleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions__padding--add');
+            question.children[0].classList.toggle('questions__arrow--rotate');
+
+            if (answer.clientHeight === 0) {
+                height = answer.scrollHeight;
+            }
+            answer.style.height = `${height}px`;
+        })
+    })
+
+    
+})();
